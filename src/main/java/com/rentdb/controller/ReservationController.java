0dc1bfae419cf9a383ce55c3ec4e49b6f29b9241
Michal Banasiak <bananiwo@gmail.com>
2022-07-05 19:25:50 +0200

    add updating reservation

--- src/main/java/com/rentdb/controller/ReservationController.java
+++ src/main/java/com/rentdb/controller/ReservationController.java
@@ -3,8 +3,10 @@ package com.rentdb.controller;
 import com.rentdb.model.Reservation;
 import com.rentdb.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -26,9 +28,15 @@ public class ReservationController {
         return reservationService.addReservation(reservation);
     }
 
-    @PutMapping("/reservations")
-    public Reservation editReservation(@RequestBody Reservation reservation) {
-        return reservationService.editReservation(reservation);
+    @PutMapping("/reservations/{reservationId}")
+    public void updateReservation(
+            @PathVariable long reservationId,
+            @RequestParam(required = false) Double cost,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate leaseStart,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate leaseEnd) {
+        reservationService.updateReservation(reservationId, cost, leaseStart, leaseEnd);
     }
 
 }
--- src/main/java/com/rentdb/service/ReservationService.java
+++ src/main/java/com/rentdb/service/ReservationService.java
@@ -5,6 +5,8 @@ import com.rentdb.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
@@ -20,16 +22,8 @@ public class ReservationService {
             throw new IllegalArgumentException("Lease is shorter than 1 day");
         }
         // check if periods of reservations with the same lease_item_id overlap
-        boolean doPeriodsOverlap = false;
-        List<Reservation> reservationsSameItemId = reservationRepository.findReservationByItemId(reservation.getItemId());
-        for (Reservation other: reservationsSameItemId) {
-            if(other.getLeaseStart().isBefore(reservation.getLeaseEnd()) &&
-                other.getLeaseEnd().isAfter(reservation.getLeaseStart())){
-                doPeriodsOverlap = true;
-            }
-        }
-        if(doPeriodsOverlap) {
-            throw new IllegalArgumentException("Item of reservation is alredy taken during this time");
+        if(isDateTaken(reservation, reservation.getLeaseStart(), reservation.getLeaseEnd())) {
+            throw new IllegalArgumentException("Item of reservation is already taken during this time");
         }
 
         return reservationRepository.save(reservation);
@@ -39,11 +33,67 @@ public class ReservationService {
         return reservationRepository.findAll();
     }
 
-    public Reservation editReservation(Reservation reservation) {
-        return reservationRepository.save(reservation);
-    }
-
     public List<Reservation> findReservationByItemId(long itemId) {
         return reservationRepository.findReservationByItemId(itemId);
+    }
+
+    @Transactional
+    public void updateReservation(long reservationId, Double cost, LocalDate leaseStart, LocalDate leaseEnd) {
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new IllegalStateException("Reservation with id " + reservationId + " does not exist"));
+
+        if(cost != null && cost > 0) {
+            reservation.setCost(cost);
+        }
+
+        // validate leastStart and leaseEnd if present
+        if(leaseStart != null && leaseEnd != null){
+            if(ChronoUnit.DAYS.between(leaseStart, leaseEnd) < 1) {
+                throw new IllegalArgumentException("Lease is shorter than 1 day");
+            }
+            if(isDateTaken(reservation, leaseStart, leaseEnd)){
+                throw new IllegalArgumentException("Item of reservation is already taken during this time");
+            }
+            reservation.setLeaseStart(leaseStart);
+            reservation.setLeaseEnd(leaseEnd);
+        }
+        else if(leaseStart != null){
+            if(ChronoUnit.DAYS.between(leaseStart, reservation.getLeaseEnd()) < 1) {
+                throw new IllegalArgumentException("Lease is shorter than 1 day");
+            }
+            if(isDateTaken(reservation, leaseStart, reservation.getLeaseEnd())) {
+                throw new IllegalArgumentException("Item of reservation is already taken during this time");
+            }
+            reservation.setLeaseStart(leaseStart);
+        }
+        else if(leaseEnd != null) {
+            if( ChronoUnit.DAYS.between(reservation.getLeaseStart(), leaseEnd) <1) {
+                throw new IllegalArgumentException("Lease is shorter than 1 day");
+            }
+            if(isDateTaken(reservation, reservation.getLeaseStart(), leaseEnd)){
+                throw new IllegalArgumentException("Item of reservation is already taken during this time");
+            }
+            reservation.setLeaseEnd(leaseEnd);
+        }
+    }
+
+    boolean ifDatesOverlap(LocalDate firstStart, LocalDate firstEnd, LocalDate secondStart, LocalDate secondEnd) {
+        return firstStart.isBefore(secondEnd) && firstEnd.isAfter(secondStart);
+    }
+
+    boolean isDateTaken(Reservation reservation, LocalDate leaseStart, LocalDate leaseEnd) {
+        boolean isTaken = false;
+        List<Reservation> reservationsSameItemId = reservationRepository.findReservationByItemId(reservation.getItemId());
+
+        for (Reservation other: reservationsSameItemId) {
+            if(other.equals(reservation)) {
+                continue;
+            }
+            if (ifDatesOverlap(leaseStart, leaseEnd,
+                    other.getLeaseStart(), other.getLeaseEnd())) {
+                isTaken = true;
+            }
+        }
+        return isTaken;
     }
 }
