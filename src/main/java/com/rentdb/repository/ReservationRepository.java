39a50107b1b48455a741b99092e6e8b396d2aa66
Michal Banasiak <bananiwo@gmail.com>
2022-07-05 14:32:24 +0200

    enable addition of new reservation

--- src/main/java/com/rentdb/repository/ReservationRepository.java
+++ src/main/java/com/rentdb/repository/ReservationRepository.java
@@ -3,6 +3,9 @@ package com.rentdb.repository;
 import com.rentdb.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    List<Reservation> findReservationByItemId(Long itemId);
 }
--- src/main/java/com/rentdb/service/ReservationService.java
+++ src/main/java/com/rentdb/service/ReservationService.java
@@ -5,6 +5,7 @@ import com.rentdb.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -14,6 +15,23 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
 
     public Reservation addReservation(Reservation reservation) {
+        // check if lease period is at least 1 day long
+        if(ChronoUnit.DAYS.between(reservation.getLeaseStart(), reservation.getLeaseEnd()) < 1) {
+            throw new IllegalArgumentException("Lease is shorter than 1 day");
+        }
+        // check if periods of reservations with the same lease_item_id overlap
+        boolean doPeriodsOverlap = false;
+        List<Reservation> reservationsSameItemId = reservationRepository.findReservationByItemId(reservation.getItemId());
+        for (Reservation other: reservationsSameItemId) {
+            if(other.getLeaseStart().isBefore(reservation.getLeaseEnd()) &&
+                other.getLeaseEnd().isAfter(reservation.getLeaseStart())){
+                doPeriodsOverlap = true;
+            }
+        }
+        if(doPeriodsOverlap) {
+            throw new IllegalArgumentException("Item of reservation is alredy taken during this time");
+        }
+
         return reservationRepository.save(reservation);
     }
 
