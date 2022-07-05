ccdac6057768194d9f54d6e691f99589eb94dcf1
Michal Banasiak <bananiwo@gmail.com>
2022-07-05 12:03:33 +0200

    add posting new reservation, get all reservations

--- src/main/java/com/rentdb/controller/ReservationController.java
+++ src/main/java/com/rentdb/controller/ReservationController.java
@@ -1,8 +1,14 @@
 package com.rentdb.controller;
 
+import com.rentdb.model.Reservation;
 import com.rentdb.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,4 +16,11 @@ public class ReservationController {
 
     public final ReservationService reservationService;
 
+    @GetMapping("/reservations")
+    public List<Reservation> getReservations() { return reservationService.getReservations(); }
+
+    @PostMapping("/reservations")
+    public Reservation addReservation(@RequestBody Reservation reservation) {
+        return reservationService.addReservation(reservation);
+    }
 }
--- src/main/java/com/rentdb/service/ReservationService.java
+++ src/main/java/com/rentdb/service/ReservationService.java
@@ -5,10 +5,19 @@ import com.rentdb.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
 
+    public Reservation addReservation(Reservation reservation) {
+        return reservationRepository.save(reservation);
+    }
+
+    public List<Reservation> getReservations() {
+        return reservationRepository.findAll();
+    }
 }
