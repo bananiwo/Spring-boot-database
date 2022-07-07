f3e12266aed8b12ad6faff3db5ffac004a2fd1a5
Michal Banasiak <bananiwo@gmail.com>
2022-07-07 12:39:35 +0200

    rewrite test

--- src/test/java/com/rentdb/controller/ReservationControllerTest.java
+++ src/test/java/com/rentdb/controller/ReservationControllerTest.java
@@ -10,14 +10,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,24 +29,25 @@ class ReservationControllerTest {
     ReservationService reservationService;
 
     Reservation reservation1 = new Reservation(0,1, 1, 1, 150.0, LocalDate.of(2000, 1, 1), LocalDate.of(2002, 1, 1));
-    Reservation reservation2 = new Reservation(1,2, 4, 3, 555.55, LocalDate.of(2003, 2, 13), LocalDate.of(2004, 3, 5));
+    Reservation reservation2 = new Reservation(1,2, 1, 1, 250.0, LocalDate.of(2001, 1, 1), LocalDate.of(2001, 1, 1));
 
     @Test
-    void addReservationTest() throws Exception {
-        List<Reservation> reservations = new ArrayList<>(Arrays.asList(reservation1, reservation2));
+    void findReservationsByItemIdTest() throws Exception {
+        List<Reservation> reservationsItemId1 = new ArrayList<>(Arrays.asList(reservation1, reservation2));
 
-        Mockito.when(reservationService.getReservations()).thenReturn(reservations);
+        Mockito.when(reservationService.findReservationByItemId(Mockito.anyLong())).thenReturn(reservationsItemId1);
 
         mockMvc.perform(MockMvcRequestBuilders
-                .get("/reservations")
+                .get("/reservations/items_leased/1")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[1].renterId", is(2)))
-                .andExpect(jsonPath("$[1].tenantId", is(4)))
-                .andExpect(jsonPath("$[1].itemId", is(3)))
-                .andExpect(jsonPath("$[0].cost", is(150.0)))
-                .andExpect(jsonPath("$[0].leaseStart", is("2000-01-01")))
-                .andExpect(jsonPath("$[0].leaseEnd", is("2002-01-01")));
+                .andExpect(jsonPath("$[0].renterId", is(1)))
+                .andExpect(jsonPath("$[0].tenantId", is(1)))
+                .andExpect(jsonPath("$[0].itemId", is(1)))
+                .andExpect(jsonPath("$[1].itemId", is(1)))
+                .andExpect(jsonPath("$[1].cost", is(250.0)))
+                .andExpect(jsonPath("$[1].leaseStart", is("2001-01-01")))
+                .andExpect(jsonPath("$[1].leaseEnd", is("2001-01-01")));
     }
 }
